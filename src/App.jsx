d0b4b82acf10a9eff60893dad47b8338da5cb904
muskan <muskantuteja714@gmail.com>
2025-11-11 01:53:36 +0530

    11/11/25

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import { h1 } from "framer-motion/client";
 
 const images = [
   "https://images.unsplash.com/photo-1761857352384-aaae23a714a4?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=764",
-  "https://images.unsplash.com/photo-1753847726685-6af24062aa9e?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=880",
   "https://plus.unsplash.com/premium_photo-1761334228337-438a180ba277?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=715",
   "https://images.unsplash.com/photo-1754481249380-3b4ada5ebc67?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=764",
   "https://plus.unsplash.com/premium_photo-1730051149650-8a1ed7b4f81f?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=715",
